9440ff42be8102bb8f9d286df5acdc57b49f40c5
AEnterprise <aenterprise2@gmail.com>
2019-12-15 13:08:03 +0100

    try to get project info

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const core = require('@actions/core');
 const fs = require('fs');
+const https = require('https');
 
-var walkSync = function (dir, filelist, addDir) {
+const walkSync = function (dir, filelist, addDir) {
     files = fs.readdirSync(dir);
     filelist = filelist || [];
     files.forEach(function (file) {
@@ -14,8 +15,39 @@ var walkSync = function (dir, filelist, addDir) {
     return filelist;
 };
 
+
+
 try {
-    console.log(walkSync(core.getInput("dir")))
+    const localFiles = walkSync(core.getInput("dir"));
+
+    const crowdin_username = core.getInput("crowdin_username");
+    const crowdin_api_key = core.getInput("crowdin_api_key");
+    const crowdin_project_identifier = core.getInput("crowdin_project_identifier");
+
+    const post = function (endpoint, handler) {
+        const options = {
+            hostname: "api.crowdin.com",
+            port: 443,
+            path: `/api/project/${crowdin_project_identifier}/${endpoint}?login=${crowdin_username}&account-key=${crowdin_api_key}&json`,
+            method: "POST"
+        };
+        const req = https.request(options, res => {
+            console.log(`statuscode: ${res.statusCode}`);
+
+            res.on('data', data => {
+                handler(JSON.parse(data))
+            })
+
+        });
+        req.on("error", e=> core.setFailed(e.message));
+        req.end();
+    };
+
+    post("info", console.log)
+
+
+
+
 } catch (error) {
     core.setFailed(error.message);
 }
