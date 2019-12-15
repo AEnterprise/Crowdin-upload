9c4e4486d701423a3ae32e18b35bad84b9f7b513
AEnterprise <aenterprise2@gmail.com>
2019-12-15 11:44:07 +0100

    try to get a list of files

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const fs = require('fs');
+
+var walkSync = function (dir, filelist) {
+    files = fs.readdirSync(dir);
+    filelist = filelist || [];
+    files.forEach(function (file) {
+        if (fs.statSync(dir + file).isDirectory()) {
+            filelist = walkSync(dir + file + '/', filelist);
+        } else {
+            filelist.push(file);
+        }
+    });
+    return filelist;
+};
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+    console.log(walkSync(core.getInput("dir")))
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
 }
